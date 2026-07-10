e2389564cd7b96e56f822f09d710380ac29dae71
Muhammad Aliyan <ali786-c@users.noreply.github.com>
2026-07-10 22:24:44 +0500

    Add Domain Transfer link to Domain menu dropdown pointing to WHMCS transfer page

--- assets/js/header.js
+++ assets/js/header.js
@@ -234,6 +234,21 @@
 </span>
 </a>
 </li>
+<li>
+<a class="navigation-2__menu-link link d-flex align-items-start gap-3" href="http://my.cloudhoste.eu/cart.php?a=add&domain=transfer">
+<span class="d-grid place-content-center w-10 h-10 bg-warning rounded-circle flex-shrink-0">
+<iconify-icon class="fs-20 text-white" icon="solar:refresh-broken"></iconify-icon>
+</span>
+<span class="d-block flex-grow-1">
+<span class="navigation-2__menu-title d-block fw-bold">
+										Domain Transfer
+									</span>
+<span class="navigation-2__menu-text line-clamp line-clamp--2 fs-14">
+										Transfer your domain to CloudHoste
+									</span>
+</span>
+</a>
+</li>
 </ul>
 </li>
 <li class="nav-item">
